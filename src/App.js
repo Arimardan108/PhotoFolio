93e2529f0d50f0d65cbf7b4ed923351e7d800376
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-27 09:20:23 +0530

    Add file

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+
+import Navbar from "./Components/Navbar";
+import Albumslist from "./Components/AlbumsList/Albumslist"
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    {/* <Albumform/> */}
+    <Albumslist/>
+    {/* <Imageslist/> */}
+    </>
+  );
+}
+
+export default App;

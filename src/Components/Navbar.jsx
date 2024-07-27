d799c1d9019afd0714132b4d0be97aa03c0c5ddd
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-27 09:24:20 +0530

    Add file

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="footer">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/4047/4047371.png"
+          alt="logo"
+        />
+        <span className="text">PhotoFolio </span>
+      </div>
+    </>
+  );
+}
